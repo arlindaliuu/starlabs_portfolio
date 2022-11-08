9970b8517c1501c30ce9ee53fc9efbe39c6ce292
Aarlind <aa51044@ubt-uni.net>
2022-11-08 16:41:35 +0100

    adding some features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-        const welcomeArr = ["Welcome to my portfolio", "Miresevini ne portfolion time"];
+        const welcomeArr = ["Junior Web Developer", "Welcome to my portfolio"];
         var x = document.getElementById('title');
         let text = '';
         let loopNum = 0;
@@ -50,21 +50,56 @@
           });
 
 
-          var fotoArray = [
-            "../../assets/img/photo.png",
-            "../../assets/img/photo1.png",
-        ]
+        
         
         var i = 0;
         
         function ndrroImg() {
-            var x = document.getElementById('slideshow').src = fotoArray[i];
-            if (i < fotoArray.length - 1) {
+            var photo = document.getElementsByClassName('slider')
+
+           
+            if (i < photo.length - 1) {
+                photo[i].style.display = 'block'
+                photo[i+1].style.display = 'none'
                 i++;
             } else {
+                photo[i].style.display = 'block'
+                photo[i-1].style.display = 'none'
                 i = 0;
             }
-            setTimeout('ndrroImg()', 3000);
+            setTimeout('ndrroImg()', 4000);
         
         }
         document.body.addEventListener('load', ndrroImg());
+
+
+        function changeTab(param){
+            let project = document.getElementsByClassName('projects')
+            let array = param.split("-")
+
+            if(array[1] == '0'){
+                project[1].classList.add('d-none');
+                project[2].classList.add('d-none');
+                project[3].classList.add('d-none');
+                project[0].classList.remove('d-none');
+            }
+            if(array[1] == '1'){
+                project[0].classList.add('d-none');
+                project[2].classList.add('d-none');
+                project[3].classList.add('d-none');
+                project[1].classList.remove('d-none');
+            }
+            if(array[1] == '2'){
+                project[0].classList.add('d-none');
+                project[1].classList.add('d-none');
+                project[3].classList.add('d-none');
+                project[2].classList.remove('d-none');
+            }
+            if(array[1] == '3'){
+                project[0].classList.add('d-none');
+                project[1].classList.add('d-none');
+                project[2].classList.add('d-none');
+                project[3].classList.remove('d-none');
+            }
+            
+        }
